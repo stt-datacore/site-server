c3dc72b0f9db6279fde1a76360b03241b99f15e1
Nathaniel Moschkin <precise.tester@gmail.com>
2023-11-07 13:23:34 -0500

    Disable attempt to get HOF; freezes system

--- app/logic/telemetry.ts
+++ app/logic/telemetry.ts
@@ -41,24 +41,26 @@ async function recordVoyageCalc({ voyagers, estimatedDuration }: { voyagers: str
 }
 
 async function getVoyageStats() {
-	const baseFilter = {
-		group: ['crewSymbol'],
-		attributes: ['crewSymbol', [Sequelize.fn('COUNT', Sequelize.col('crewSymbol')), 'crewCount'], [Sequelize.fn('AVG', Sequelize.col('estimatedDuration')), 'averageDuration']],
-	} as any;
-	const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
-	const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
-	const one80DaysAgo = new Date(Date.now() - 180 * 24 * 60 * 60 * 1000);
-	// const allTime = await VoyageRecord.findAll({ ...baseFilter });
-	// const allSinceNewTelemetry = await VoyageRecord.findAll({ ...baseFilter, where: { estimatedDuration: { [Op.ne]: null } } });
-	const lastSevenDays = await VoyageRecord.findAll({ ...baseFilter, where: { voyageDate: { [Op.gt]: sevenDaysAgo } } });
-	const lastThirtyDays = await VoyageRecord.findAll({ ...baseFilter, where: { voyageDate: { [Op.gt]: thirtyDaysAgo } } });
-	const lastSixMonths = await VoyageRecord.findAll({ ...baseFilter, where: { voyageDate: { [Op.gt]: one80DaysAgo } } });
+	// This freezes the system.	
+	return {};
+	// const baseFilter = {
+	// 	group: ['crewSymbol'],
+	// 	attributes: ['crewSymbol', [Sequelize.fn('COUNT', Sequelize.col('crewSymbol')), 'crewCount'], [Sequelize.fn('AVG', Sequelize.col('estimatedDuration')), 'averageDuration']],
+	// } as any;
+	// const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+	// const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+	// const one80DaysAgo = new Date(Date.now() - 180 * 24 * 60 * 60 * 1000);
+	// // const allTime = await VoyageRecord.findAll({ ...baseFilter });
+	// // const allSinceNewTelemetry = await VoyageRecord.findAll({ ...baseFilter, where: { estimatedDuration: { [Op.ne]: null } } });
+	// const lastSevenDays = await VoyageRecord.findAll({ ...baseFilter, where: { voyageDate: { [Op.gt]: sevenDaysAgo } } });
+	// const lastThirtyDays = await VoyageRecord.findAll({ ...baseFilter, where: { voyageDate: { [Op.gt]: thirtyDaysAgo } } });
+	// const lastSixMonths = await VoyageRecord.findAll({ ...baseFilter, where: { voyageDate: { [Op.gt]: one80DaysAgo } } });
 	
-	return {
-		// allTime,
-		// allSinceNewTelemetry,
-		lastSixMonths,
-		lastSevenDays,		
-		lastThirtyDays
-	}
+	// return {
+	// 	// allTime,
+	// 	// allSinceNewTelemetry,
+	// 	lastSixMonths,
+	// 	lastSevenDays,		
+	// 	lastThirtyDays
+	// }
 }
