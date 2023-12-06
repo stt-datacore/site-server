8e183426ce85ae1e845963b0d42b4ec009ac0b48
Nathaniel Moschkin <precise.tester@gmail.com>
2023-12-05 21:45:56 -0500

    new telemetry reporting

--- app/logic/telemetry.ts
+++ app/logic/telemetry.ts
@@ -1,3 +1,4 @@
+import { Op } from 'sequelize';
 import { Voyage } from '../models/VoyageRecord';
 
 export async function recordTelemetryDB(type: string, data: any) {
@@ -47,26 +48,91 @@ async function recordVoyageCalc({ voyagers, estimatedDuration }: { voyagers: str
 }
 
 async function getVoyageStats() {
-	// This freezes the system.	
-	return {};
-	// const baseFilter = {
-	// 	group: ['crewSymbol'],
-	// 	attributes: ['crewSymbol', [Sequelize.fn('COUNT', Sequelize.col('crewSymbol')), 'crewCount'], [Sequelize.fn('AVG', Sequelize.col('estimatedDuration')), 'averageDuration']],
-	// } as any;
-	// const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
-	// const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
-	// const one80DaysAgo = new Date(Date.now() - 180 * 24 * 60 * 60 * 1000);
-	// // const allTime = await VoyageRecord.findAll({ ...baseFilter });
-	// // const allSinceNewTelemetry = await VoyageRecord.findAll({ ...baseFilter, where: { estimatedDuration: { [Op.ne]: null } } });
-	// const lastSevenDays = await VoyageRecord.findAll({ ...baseFilter, where: { voyageDate: { [Op.gt]: sevenDaysAgo } } });
-	// const lastThirtyDays = await VoyageRecord.findAll({ ...baseFilter, where: { voyageDate: { [Op.gt]: thirtyDaysAgo } } });
-	// const lastSixMonths = await VoyageRecord.findAll({ ...baseFilter, where: { voyageDate: { [Op.gt]: one80DaysAgo } } });
+	const one80DaysAgo = new Date(Date.now() - 180 * 24 * 60 * 60 * 1000);
 	
-	// return {
-	// 	// allTime,
-	// 	// allSinceNewTelemetry,
-	// 	lastSixMonths,
-	// 	lastSevenDays,		
-	// 	lastThirtyDays
-	// }
+	const seats = [
+		'command_skill', 
+		'command_skill', 
+		'diplomacy_skill', 
+		'diplomacy_skill', 
+		'security_skill', 
+		'security_skill', 
+		'engineering_skill', 
+		'engineering_skill', 
+		'science_skill', 
+		'science_skill', 
+		'medicine_skill',
+		'medicine_skill' 
+	];
+
+	const records = await Voyage.findAll({ where: { voyageTime: { [Op.gte]: one80DaysAgo }}});
+	const output = {} as any;
+
+	const dsets = [{
+		date: new Date(Date.now() - (180 * 24 * 60 * 60 * 1000)),
+		file: "lastSixMonths"
+	},
+	{
+		date: new Date(Date.now() - (30 * 24 * 60 * 60 * 1000)),
+		file: "lastThirtyDays"
+	},
+	{
+		date: new Date(Date.now() - (7 * 24 * 60 * 60 * 1000)),
+		file: "lastSevenDays"
+	}];
+
+	for (let dset of dsets) {
+		let { date: d, file: fn } = dset;
+		console.log(`From ${d} as '${fn}'...`);
+		let results = records.filter(r => r.voyageDate.getTime() >= dset.date.getTime());
+
+		type CrewPeople = {
+			crewSymbol: string,
+			seats: { seat: string, count: number, avgTime: number }[],
+			averageDuration: number,
+			firstVoyage: Date,
+			mostRecentVoyage: Date,
+			crewCount: number;
+		}
+	
+		const cp = {} as { [key: string]: CrewPeople };
+	
+		for (let res of results) {
+			let seat = 0;
+			if (!res.estimatedDuration) continue;
+			for (let c of res.crew) {
+				cp[c] ??= {
+					crewSymbol: c,
+					seats: [],
+					averageDuration: 0,
+					firstVoyage: res.voyageDate,
+					mostRecentVoyage: res.voyageDate,
+					crewCount: 0
+				};
+				cp[c].crewCount++;
+				cp[c].mostRecentVoyage = res.voyageDate;
+				cp[c].averageDuration = ((cp[c].averageDuration * cp[c].seats.length) + res.estimatedDuration) / (cp[c].seats.length + 1);
+				let currseat = cp[c].seats.find(s => s.seat === seats[seat]);
+				if (currseat) {
+					currseat.avgTime = ((currseat.avgTime * currseat.count) + res.estimatedDuration) / (currseat.count + 1);
+					currseat.count++;
+				}
+				else {
+					cp[c].seats.push({
+						seat: seats[seat],
+						count: 1,
+						avgTime: res.estimatedDuration
+					})
+				}
+				seat++;
+			}
+		}
+
+		let finalOut = Object.values(cp);
+
+		finalOut.sort((a, b) => b.crewCount - a.crewCount);
+		output[dset.file] = finalOut;
+	}
+
+	return output;
 }
