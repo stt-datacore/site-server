1654429f8798c321ef1cd324d46deb2deb1ea14e
Nathaniel Moschkin <precise.tester@gmail.com>
2023-12-05 20:38:22 -0500

    Add new model to sequelize init

--- app/sequelize.ts
+++ app/sequelize.ts
@@ -3,11 +3,11 @@ import { Sequelize } from 'sequelize-typescript';
 import { User } from './models/User';
 import { Profile } from './models/Profile';
 import { Comment } from './models/Comment';
-import { VoyageRecord } from './models/VoyageRecord';
+import { Voyage, VoyageRecord } from './models/VoyageRecord';
 
 require('dotenv').config();
 
 export const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING!, {
-	models: [User, Profile, Comment, VoyageRecord],
+	models: [User, Profile, Comment, VoyageRecord, Voyage],
 	logging: false
 });
