0f865aed2351e4d2e2d6e99997a929d7a7961932
Nathaniel Moschkin <precise.tester@gmail.com>
2024-02-10 22:18:03 -0500

    rewrite load fleet

--- app/logic/stttools.ts
+++ app/logic/stttools.ts
@@ -3,19 +3,20 @@ import fetch from 'node-fetch';
 import { Logger } from './logger';
 
 // Facebook STT client ID
-const CLIENT_ID = '322613001274224';
+const CLIENT_ID = '1425f8c5-b07f-45ba-b490-3dfd4561d5cf';
 
-export async function getSTTToken() {
+export async function getSTTToken(username?: string, password?: string) {
   Logger.info('Fetching new STT API token');
+
   const params = new URLSearchParams();
-  params.append('username', process.env.STT_BOT_USERNAME!);
-  params.append('password', process.env.STT_BOT_PASSWORD!);
+  params.append('username', username ?? process.env.STT_BOT_USERNAME!);
+  params.append('password', password ?? process.env.STT_BOT_PASSWORD!);
   params.append('grant_type', 'password');
   params.append('client_id', CLIENT_ID);
 
 	const data = await fetch(
     'https://thorium.disruptorbeam.com/oauth2/token',
-    { method: 'POST', body: params }
+    { method: 'POST', body: params, headers: { 'Content-type': 'application/x-www-form-urlencoded' } }
   )
     .then((res) => res.json());
 
