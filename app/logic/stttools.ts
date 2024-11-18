35e36d849e694019925a288c7199ce0a7d5e3c4e
Nathaniel Moschkin <precise.tester@gmail.com>
2024-11-17 19:49:22 -0500

    code cleanup.

--- app/logic/stttools.ts
+++ app/logic/stttools.ts
@@ -7,25 +7,26 @@ import { Logger } from './logger';
 const CLIENT_ID = '322613001274224';
 
 export async function getSTTToken(username?: string, password?: string) {
-  Logger.info('Fetching new STT API token');
+    Logger.info('Fetching new STT API token');
 
-  const params = new URLSearchParams();
-  params.append('username', username ?? process.env.STT_BOT_USERNAME!);
-  params.append('password', password ?? process.env.STT_BOT_PASSWORD!);
-  params.append('grant_type', 'password');
-  params.append('client_id', CLIENT_ID);
+    const params = new URLSearchParams();
 
-  const data = await fetch(
-    'https://thorium.disruptorbeam.com/oauth2/token',
-    { method: 'POST', body: params, headers: { 'Content-type': 'application/x-www-form-urlencoded' } }
-  )
-    .then((res) => res.json());
+    params.append('username', username ?? process.env.STT_BOT_USERNAME!);
+    params.append('password', password ?? process.env.STT_BOT_PASSWORD!);
+    params.append('grant_type', 'password');
+    params.append('client_id', CLIENT_ID);
 
-  if (data?.access_token) {
-    Logger.info('New token fetched successfully');
-    return data.access_token as string;
-  } else {
-    Logger.info('Failed to fetch token', data);
-    throw new Error(`Failed to fetch token: ${data}`);
-  }
+    const data = await fetch(
+        'https://thorium.disruptorbeam.com/oauth2/token',
+        { method: 'POST', body: params, headers: { 'Content-type': 'application/x-www-form-urlencoded' } }
+    )
+        .then((res) => res.json());
+
+    if (data?.access_token) {
+        Logger.info('New token fetched successfully');
+        return data.access_token as string;
+    } else {
+        Logger.info('Failed to fetch token', data);
+        throw new Error(`Failed to fetch token: ${data}`);
+    }
 }
